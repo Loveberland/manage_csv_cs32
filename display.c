a41eb71bc223c789cc4b5168812d0e0e681d29db
Loveberland <lovekittipong12@gmail.com>
2026-05-05 21:31:27 +0700

    add display_all function, rebuild output when use display

--- display.c
+++ display.c
@@ -5,6 +5,91 @@
 #include "time_manager.h"
 
 int display_all() {
+	char line[4096], n_line[4096], p_title[4096], n_title[4096];
+	char p_title_copy[4096], n_title_copy[4096], p_id_copy[16];
+	char *saveptr1, *saveptr2;
+
+	FILE *p_file = fopen("p_data.csv", "r");
+	if (p_file == NULL) {
+		fprintf(stderr, "can't open p_data.csv\n");
+		return (1);
+	}
+
+	FILE *n_file = fopen("n_data.csv", "r");
+	if (n_file == NULL) {
+		fprintf(stderr, "can't open n_data.csv\n");
+		fclose(p_file);
+		return (1);
+	}
+
+	fgets(p_title, sizeof(p_title), p_file);
+	p_title[strcspn(p_title, "\n")] = '\0';
+	fgets(n_title, sizeof(n_title), n_file);
+	n_title[strcspn(n_title, "\n")] = '\0';
+
+	int is_p_print = 0, is_n_print = 0;
+
+	while (fgets(line, sizeof(line), p_file)) {
+		is_p_print = is_n_print = 0;
+		line[strcspn(line, "\n")] = '\0';
+		char *token = strtok_r(line, ",", &saveptr1);
+		if (token == NULL) continue;
+		
+		strcpy(p_title_copy, p_title);
+		char *p_token = strtok_r(p_title_copy, ",", &saveptr2);
+		if (!is_p_print) {
+			while (p_token != NULL) {
+				fprintf(stdout, "%s, ", p_token);
+				p_token = strtok_r(NULL, ",", &saveptr2);
+			}
+			is_p_print = 1;
+		}
+		putchar('\n');
+
+		strcpy(p_id_copy, token);
+		while (token != NULL) {
+			fprintf(stdout, "%s, ", token);
+			token = strtok_r(NULL, ",", &saveptr1);
+		}
+		putchar('\t');	
+
+		fseek(n_file, 0, SEEK_SET);
+		fgets(n_line, sizeof(n_line), n_file);
+		while (fgets(n_line, sizeof(n_line), n_file)) {
+			n_line[strcspn(n_line, "\n")] = '\0';
+			char *link = strrchr(n_line, ',');
+
+			if (link == NULL) continue;
+			link++;
+			while (*link == ' ') link++;
+			char *cr = strchr(link, '\r');
+			if (cr != NULL) *cr = '\0';
+			if (strcmp(p_id_copy, link) != 0) continue;
+			if (!is_n_print) {
+				strcpy(n_title_copy, n_title);
+				char *n_token = strtok_r(n_title_copy, ",", &saveptr2);
+				fprintf(stdout, "\n\t");
+				while (n_token != NULL) {
+					fprintf(stdout, "%s, ", n_token);
+					n_token = strtok_r(NULL, ",", &saveptr2);
+				}
+				is_n_print = 1;
+				putchar('\n');
+			}
+			putchar('\t');
+			char *n = strtok_r(n_line, ",", &saveptr2);
+			while (n != NULL) {
+				fprintf(stdout, "%s, ", n);
+				n = strtok_r(NULL, ",", &saveptr2);
+			}
+			putchar('\n');
+		}	
+			fprintf(stdout, "\n\n\n\n\n");
+	}
+
+	fclose(p_file);
+	fclose(n_file);
+
 	return (0);
 }
 
@@ -43,17 +128,17 @@ int display_one() {
 			strcpy(p_title_copy, p_title);
 			char *p_token = strtok_r(p_title_copy, ",", &saveptr2);
 			while (p_token != NULL) {
-				fprintf(stdout, "%-15s", p_token);
+				fprintf(stdout, "%s, ", p_token);
 				p_token = strtok_r(NULL, ",", &saveptr2);
 			}
 			putchar('\n');
 
-			fprintf(stdout, "%-15s", token);
+			fprintf(stdout, "%s, ", token);
 
 			// print p_data that match id
 			token = strtok_r(NULL, ",", &saveptr1);
 			while (token != NULL) {
-				fprintf(stdout, "%-15s", token);
+				fprintf(stdout, "%s, ", token);
 				token = strtok_r(NULL, ",", &saveptr1);
 			}
 
@@ -73,7 +158,7 @@ int display_one() {
 					char *n_token = strtok_r(n_title_copy, ",", &saveptr2);
 					fprintf(stdout, "\n\t");
 					while (n_token != NULL) {
-						fprintf(stdout, "%-15s", n_token);
+						fprintf(stdout, "%s, ", n_token);
 						n_token = strtok_r(NULL, ",", &saveptr2);		
 					}
 					is_print = 1;
@@ -82,7 +167,7 @@ int display_one() {
 				fprintf(stdout, "\t");
 				char *n = strtok_r(n_line, ",", &saveptr2);
 				while (n != NULL) {	// print
-					fprintf(stdout, "%-15s", n);
+					fprintf(stdout, "%s, ", n);
 					n = strtok_r(NULL, ",", &saveptr2);
 				}
 				putchar('\n');
@@ -111,12 +196,12 @@ int display_one() {
 			strcpy(title_copy, title);
 			char *tmp = strtok_r(title, ",", &saveptr2);
 			while (tmp != NULL) {
-				fprintf(stdout, "%-15s", tmp);
+				fprintf(stdout, "%s, ", tmp);
 				tmp = strtok_r(NULL, ",", &saveptr2);
 			}
 			putchar('\n');
 			while (token != NULL) {
-				fprintf(stdout, "%-15s", token);
+				fprintf(stdout, "%s, ", token);
 				token = strtok_r(NULL, ",", &saveptr1);
 			}
 			putchar('\n');
